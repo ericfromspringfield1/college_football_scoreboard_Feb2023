df8875a2beb2b31f7571096b137905a5f3c8889d
Eric Taylor <ericfromspringfield@gmail.com>
2023-02-24 23:16:52 -0600

    headers

--- scripts/plays.js
+++ scripts/plays.js
@@ -9,7 +9,19 @@ let gameUrl
 // Need a button to refresh just the game that has been clicked and a button to take you back to the list of games.
 
 const fetchScoreboard = async () => {
-    const jsonData = await fetch (scoreboardURL)
+    const jsonData = await fetch (scoreboardURL, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url)
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
     const data = await jsonData.json()
     console.log(data)
 
