0593393281d5557116b34ff3eee4be81b7ad1f66
Eric Taylor <ericfromspringfield@gmail.com>
2023-02-25 22:56:09 -0600

    added referrer with different value

--- scripts/plays.js
+++ scripts/plays.js
@@ -26,9 +26,8 @@ const fetchScoreboard = async () => {
             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
         },
         redirect: "follow", // manual, *follow, error
-        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url)
+        referrerPolicy: "no-referrer-when-downgrade", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url)
     });
-    console.log(jsonData)
     const data = await jsonData.json()
     console.log(data)
     
